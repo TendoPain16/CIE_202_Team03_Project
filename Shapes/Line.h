857003453ea6e2a6a6f10847a10a04ee47a5768c
Remesi <s-ahmed.elremesy@zewailcity.edu.eg>
2022-11-26 00:57:11 +0200

    Add line class

--- Shapes/Line.h
+++ Shapes/Line.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "Shape.h"
+
+class Line : public shape
+{
+private:
+	Point Corner1;
+	Point Corner2;
+public:
+	Line(Point, Point, GfxInfo ShapeGfxInfo);
+	virtual ~Line();
+	virtual void Draw(GUI* pUI) const;
+};
